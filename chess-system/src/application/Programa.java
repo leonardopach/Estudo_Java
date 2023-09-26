6e08cf6c02de82077b907fe3c0986af0a9f69b7a
leonardoP <pachecodossantos17@gmail.com>
2023-09-26 08:16:22 -0300

    First class: Position

---
+++ chess-system/src/application/Programa.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Position;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Position position = new Position(3, 5);
+
+		System.out.println(position);
+	}
+}
